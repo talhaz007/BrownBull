8c29b2d40a340bf436eabdfd8b060160aec1df8f
talhaz007 <talhazafar0007@gmail.com>
2025-04-12 01:47:09 +0500

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'BrownBullPvtLtd',
+  description: 'Unlock the power of smart trading strategies with Brown Bull! We help you make informed decisions, manage risks, and grow your investments in the dynamic world.',
+  generator: 'ConceptSol',
 }
 
 export default function RootLayout({
